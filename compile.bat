9faf91f2c472384cee420ed1dd5a41445552ef04
MatteoSanti-prog <10665164@polimi.it>
2023-11-07 16:18:27 +0100

    Revert "Test"
    This reverts commit 78e69b638a78787e6341006446bce3a348d0b952.

--- compile.bat
+++
@@ -1,3 +0,0 @@
-cd "C:\Users\santi\Documents\Visual Studio 2022\Projects\Computer_Graphics_Project_2022-2023"
-cl /MD /I "C:\Users\santi\Documents\Visual Studio 2022\Libraries\glfw-3.3.8.bin.WIN64\include" /I .\headers\ /I C:\VulkanSDK\1.3.239.0\Include /std:c++17 /EHsc .\Main.cpp gdi32.lib opengl32.lib kernel32.lib user32.lib shell32.lib glfw3.lib vulkan-1.lib /link /LIBPATH:"C:\Users\santi\Documents\Visual Studio 2022\Libraries\glfw-3.3.8.bin.WIN64\lib-vc2022" /LIBPATH:C:\VulkanSDK\1.3.239.0\Lib
-pause
