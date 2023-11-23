eb592560a00662777d810f276b78cfc649ce96e3
MatteoSanti-prog <10665164@polimi.it>
2023-11-23 22:37:07 +0100

    Update
    Small corrections and useless code deleted. PARAMETER TUNING and LATERAL FRICTION are not completed!

--- Controller.hpp
+++ Controller.hpp
@@ -67,13 +67,13 @@ void FreeCam(float deltaT, glm::vec3 m, glm::vec3 r, glm::mat4& ViewMatrix, glm:
 	/*Update value for dumping*/
 	CamPosOld = LocalCamPos;
 
-	/*Create a World Matrix for the car*/
-	WorldMatrix = glm::translate(glm::mat4(1.0), CarPos) * glm::rotate(glm::mat4(1.0), CarYaw, glm::vec3(0, 1, 0));
-
 	/*Update the output variables (car position and car direction are fixed)*/
 	CamPos = LocalCamPos;
 	CarPos = glm::vec3(0.0f);
 	CarYaw = glm::radians(180.0f);
+
+	/*Create a World Matrix for the car*/
+	WorldMatrix = glm::translate(glm::mat4(1.0), CarPos) * glm::rotate(glm::mat4(1.0), CarYaw, glm::vec3(0, 1, 0));
 }
 
 /*
@@ -105,35 +105,36 @@ void GameLogic(float deltaT, glm::vec3 m, glm::vec3 r, glm::mat4& ViewMatrix, gl
 	const float MinPitch = glm::radians(10.0f);
 	const float MaxPitch = glm::radians(30.0f);
 
-	/*Constants used to bind translational speed, acceleration and deceleration of the car*/
-	const float MaxSpeedForward = 15.0f;
-	const float MinSpeedBackward = -3.0f;	
-	const float AccFactorForward = 5.0f;
-    const float AccFactorBackward = -2.5f;
-	const float FrictionFactor = 3.5f;
-	const float MotorBrakeFactor = 8.5f;
+	/*Constants used to bind translational speed and acceleration of the car*/
+	const float SpeedThreshold = 0.12f;
+	const float MaxMovSpeedForward = 15.0f;
+	const float MaxMovSpeedBackward = 6.0f;	
+	const float MovAccFactorForward = 5.0f;
+    const float MovAccFactorBackward = 2.5f;
+
+	/*Constant used to implement the motor brake (during translational movement)*/
+	const float MotorBrakeFactor = 0.6f;
+
+	/*Constant used to implement power steering (during rotational movement)*/
+	const float PowerSteeringFactor = 3.5f;
 	
-	/*Constants used to bind rotational speed, acceleration and deceleration of the car*/
-	const float MaxRotSpeed = glm::radians(120.0f);
-    const float RotDecFactor = 1.0f;
-    const float PowerSteeringFactor = 3.5f;
-    const float RotAccFactor = 4.5f;
+	/*Constants used to bind rotational speed and acceleration of the car*/
+	const float MaxRotSpeed = glm::radians(80.0f);
+	const float RotAccFactorForward = 3.5f;
+	const float RotAccFactorBackward = 1.0f;
     
-	/*Static variables used to store the current transaltional and rotational speed of the camera*/
+	/*Static variables used to store the current transaltional and rotational speed of the car*/
 	static float MovSpeed = 0.0f;
 	static float RotSpeed = glm::radians(0.0f);
 
-	/*Floating point variables used to store translational and rotational factors of acceleration and deceleration*/
-	float MovAccelerationFactor, MovDecelerationFactor, RotAccelerationFactor, RotDecelerationFactor;
+	/*Static variable used to store the transaltional speed of the car in the previous frame*/
+	static float LastMovSpeed = 0.0f;
 
-	/*Floating point variable used to store acceleration*/
-	float Acceleration;
+	/*Floating point variables used to store translational and rotational accelerations (and decelerations)*/
+	float MovAcceleration, MovDeceleration, RotAcceleration, RotDeceleration;
 
-	/*Floating point variable used to store normalized movement speed*/
+	/*Floating point variable used to store normalized translational speed*/
 	float NormMovSpeed;
-
-	/*Floating point variable used to store the parameter used for interpolation*/
-	float Amount;
 
 	/*Static variables used to store current positon and direction both for the car and for the camera*/
 	static float LocalCarYaw = StartingDirection;
@@ -141,7 +142,8 @@ void GameLogic(float deltaT, glm::vec3 m, glm::vec3 r, glm::mat4& ViewMatrix, gl
 	static float CamPitch = glm::radians(10.0f);
 
 	/*Static variables used to perform dumping*/
-	static glm::vec3 CamPosOld = StartingPosition, CamPosNew;
+	static glm::vec3 CamPosOld = StartingPosition;
+	static glm::vec3 CamPosNew;
 
 	/*3D vector used to store the point of the car followed by the camera*/
 	glm::vec3 Target;
@@ -153,38 +155,49 @@ void GameLogic(float deltaT, glm::vec3 m, glm::vec3 r, glm::mat4& ViewMatrix, gl
 	CamPitch += -RotSpeed * r.x * deltaT;
 	CamPitch = CamPitch < MinPitch ? MinPitch :
 		(CamPitch > MaxPitch ? MaxPitch : CamPitch);
+
+	/*STEERING OF THE CAR*/
+
+	/*Update the rotational accelerations (the update depends on the status of the key used for rotation)*/
+    RotDeceleration = r.y != 0 ? 0.0f : -RotSpeed * PowerSteeringFactor; 
     
+	/*Update the rotational acceleration and bind the rotational speed of the car (the update depends on the status of the key used for translation*/
+	RotAcceleration = m.z > 0.0f ? RotAccFactorForward : RotAccFactorBackward;
+	
+	/*Update the rotational speed*/
+	RotSpeed = RotSpeed > MaxRotSpeed ? MaxRotSpeed :
+		(RotSpeed < -MaxRotSpeed ? -MaxRotSpeed : RotSpeed + (r.y * RotAcceleration + RotDeceleration) * deltaT);
+
+	/*TRANSLATION OF THE CAR*/
+
 	/*Update the normalized translational speed (the update depends on the sign of the translational speed of the car)*/
-    NormMovSpeed = MovSpeed >= 0 ? MovSpeed/MaxSpeedForward : MovSpeed/MinSpeedBackward;
+	NormMovSpeed = MovSpeed >= 0 ? MovSpeed / MaxMovSpeedForward : MovSpeed / MaxMovSpeedBackward;
 
-	/*Update the translational factors (the update depends on the status of the key used for acceleration)*/
-    MovDecelerationFactor = m.z != 0 ? -NormMovSpeed * FrictionFactor : -NormMovSpeed * (FrictionFactor + MotorBrakeFactor);
-	MovAccelerationFactor = m.z > 0.0f ? AccFactorForward :
-		(m.z < 0.0f ? AccFactorBackward : 0.0f);
+	/*Update the translational accelerations (the update depends on the status of the key used for acceleration)*/
+	MovDeceleration = m.z != 0 ? 0.0f : -MovSpeed * MotorBrakeFactor;
+	MovAcceleration = m.z > 0.0f ? MovAccFactorForward :
+		(m.z < 0.0f ? MovAccFactorBackward : 0.0f);
 
-	/*Update the acceleration of the car*/
-	Acceleration = MovAccelerationFactor + MovDecelerationFactor;
-    
 	/*Update and bind the translational speed of the car*/
-	MovSpeed = MovSpeed > MaxSpeedForward ? MaxSpeedForward :
-		(MovSpeed < MinSpeedBackward ? MinSpeedBackward : MovSpeed + Acceleration * deltaT);
-    
-	/*Update the position of the car*/
-	LocalCarPos += uz * MovSpeed * deltaT;
+	MovSpeed = MovSpeed > MaxMovSpeedForward ? MaxMovSpeedForward :
+		(MovSpeed < -MaxMovSpeedBackward ? -MaxMovSpeedBackward : MovSpeed + (m.z * MovAcceleration + MovDeceleration) * deltaT);
 
-	/*Update the rotational factors (the update depends on the status of the key used for rotation)*/
-    RotDecelerationFactor = r.y != 0 ? -RotSpeed * RotDecFactor : -RotSpeed * (RotDecFactor + PowerSteeringFactor); 
-    RotAccelerationFactor = r.y * RotAccFactor + RotDecelerationFactor;
-    
-	/*Update and bind the rotational speed of the car*/
-    RotSpeed = RotSpeed > MaxRotSpeed ? MaxRotSpeed :
-        (RotSpeed < -MaxRotSpeed ? -MaxRotSpeed : RotSpeed + RotAccelerationFactor * deltaT);
-    
-	/*Update the parameter used for interpolation*/
-    Amount = MovSpeed >= 0 ? (MovSpeed/MaxSpeedForward) : (MovSpeed/MinSpeedBackward);
+	/*QUI POSSIAMO PENSARE DI AGGIUNGERE FRIZIONE LATERALE*/
+	/*Del tipo: LateralFriction = r.y * LateralFrictionFactor (poi gestisci il segno della frizione laterale perchè deve sempre
+	essere sotttratto alla velocità corrente)*/
+
+	/*If the translational speed reaches a certain threshold, its value becomes zero*/
+	MovSpeed = MovSpeed < SpeedThreshold && LastMovSpeed >= SpeedThreshold ? 0.0f :
+		(MovSpeed > -SpeedThreshold && LastMovSpeed <= -SpeedThreshold ? 0.0f : MovSpeed);
+
+	/*Update the translational speed of the previous frame*/
+	LastMovSpeed = MovSpeed;
     
 	/*Update the direction of the car using an interpolation method in order to have a more realistic movement*/
-    LocalCarYaw = glm::mix(LocalCarYaw, LocalCarYaw - RotSpeed * deltaT, Amount);
+    LocalCarYaw = glm::mix(LocalCarYaw, LocalCarYaw - RotSpeed * deltaT, NormMovSpeed);
+
+	/*Update the position of the car*/
+	LocalCarPos += uz * MovSpeed * deltaT;
 
 	/*Create a World Matrix for the car*/
 	WorldMatrix = glm::translate(glm::mat4(1.0), LocalCarPos) * glm::rotate(glm::mat4(1.0), LocalCarYaw, glm::vec3(0, 1, 0));
@@ -200,11 +213,11 @@ void GameLogic(float deltaT, glm::vec3 m, glm::vec3 r, glm::mat4& ViewMatrix, gl
 	/*Update value for dumping*/
 	CamPosOld = LocalCamPos;
 
-	/*Create again a World Matrix for the car where the model is rotated consistently with respect to the view*/
-	WorldMatrix = glm::translate(glm::mat4(1.0), LocalCarPos) * glm::rotate(glm::mat4(1.0), glm::radians(180.0f) + LocalCarYaw, glm::vec3(0, 1, 0));
-
 	/*Update the output variables (car position and car direction are fixed)*/
 	CamPos = LocalCamPos;
 	CarPos = LocalCarPos;
 	CarYaw = LocalCarYaw;
+
+	/*Create again a World Matrix for the car where the model is rotated consistently with respect to the view*/
+	WorldMatrix = glm::translate(glm::mat4(1.0), LocalCarPos) * glm::rotate(glm::mat4(1.0), glm::radians(180.0f) + LocalCarYaw, glm::vec3(0, 1, 0));
 }
