775e78d77c3147b81bd8ecf30ee05e7a4a858196
AndreaGrassi98 <79790392+AndreaGrassi98@users.noreply.github.com>
2023-11-28 14:22:51 +0100

    -

--- main_environment.hpp
+++ main_environment.hpp
@@ -5,16 +5,16 @@
 
 void A16::createEnvironment(std::vector<VertexEnv> &vPos, std::vector<uint32_t> &vIdx) {
 	//X Y Z
-	vPos.push_back({{-15,15,0}, {0,1,0}, {0,1,0}});
+	vPos.push_back({{-15,15,0}, {0,0,-1}, {0,1,0}});
 	//vPos.push_back(-15); vPos.push_back(15); vPos.push_back(0); //A	//0
-	vPos.push_back({{15,15,0}, {0,1,0}, {0,1,0}});
+	vPos.push_back({{15,15,0}, {0,0,-1}, {0,1,0}});
 	//vPos.push_back(15); vPos.push_back(15); vPos.push_back(0); //B
-	vPos.push_back({{-15,-15,0}, {0,1,0}, {0,1,0}});
+	vPos.push_back({{-15,-15,0}, {0,0,-1}, {0,1,0}});
 	//vPos.push_back(-15); vPos.push_back(-15); vPos.push_back(0); //C
-	vPos.push_back({{15,-15,0}, {0,1,0}, {0,1,0}});
+	vPos.push_back({{15,-15,0}, {0,0,-1}, {0,1,0}});
 	//vPos.push_back(15); vPos.push_back(-15); vPos.push_back(0); //D
 
-	vPos.push_back({{8,-8,0}, {0,1,0}, {0,0,1}});
+	vPos.push_back({{8,-8,1}, {0,1,0}, {0,0,1}});
 	//vPos.push_back(8); vPos.push_back(-8); vPos.push_back(1); //E	//4
 	vPos.push_back({{6,-8,1}, {0,1,0}, {0,0,1}});
 	//vPos.push_back(6); vPos.push_back(-8); vPos.push_back(1); //H
@@ -40,39 +40,39 @@ void A16::createEnvironment(std::vector<VertexEnv> &vPos, std::vector<uint32_t> 
 	//vPos.push_back(-5.4); vPos.push_back(7.4); vPos.push_back(1); //P
 	
 	//LOWER PART
-	vPos.push_back({{-15,15,-1}, {0,1,0}, {0,0,1}});
+	vPos.push_back({{-15,15,0}, {0,1,0}, {0,0,1}});
 	//vPos.push_back(-15); vPos.push_back(15); vPos.push_back(-1); //A'	//15
-	vPos.push_back({{15,15,-10}, {0,1,0}, {0,0,1}});
+	vPos.push_back({{15,15,0}, {0,1,0}, {0,0,1}});
 	//vPos.push_back(15); vPos.push_back(15); vPos.push_back(-10); //B'
-	vPos.push_back({{-15,-15,-10}, {0,1,0}, {0,0,1}});
+	vPos.push_back({{-15,-15,0}, {0,1,0}, {0,0,1}});
 	//vPos.push_back(-15); vPos.push_back(-15); vPos.push_back(-10); //C'
-	vPos.push_back({{15,-15,-10}, {0,1,0}, {0,0,1}});
+	vPos.push_back({{15,-15,0}, {0,1,0}, {0,0,1}});
 	//vPos.push_back(15); vPos.push_back(-15); vPos.push_back(-10); //D'
 
-	vPos.push_back({{8,-8,-1}, {0,1,0}, {0,0,1}});
-	//vPos.push_back(8); vPos.push_back(-8); vPos.push_back(-1); //E'	//19
-	vPos.push_back({{6,-8,-1}, {0,1,0}, {0,0,1}});
-	//vPos.push_back(6); vPos.push_back(-8); vPos.push_back(-1); //H'
-	vPos.push_back({{6,0,-1}, {0,1,0}, {0,0,1}});
-	//vPos.push_back(6); vPos.push_back(0); vPos.push_back(-1); //G'
-	vPos.push_back({{8,0,-1}, {0,1,0}, {0,0,1}});
-	//vPos.push_back(8); vPos.push_back(0); vPos.push_back(-1); //'
+	vPos.push_back({{8,-8, 0}, {0,1,0}, {0,0,1}});
+	//vPos.push_back(8); vPos.push_back(-8); vPos.push_back(0); //E'	//19
+	vPos.push_back({{6,-8, 0}, {0,1,0}, {0,0,1}});
+	//vPos.push_back(6); vPos.push_back(-8); vPos.push_back(0); //H'
+	vPos.push_back({{6,0,0}, {0,1,0}, {0,0,1}});
+	//vPos.push_back(6); vPos.push_back(0); vPos.push_back(0); //G'
+	vPos.push_back({{8,0,0}, {0,1,0}, {0,0,1}});
+	//vPos.push_back(8); vPos.push_back(0); vPos.push_back(0); //'
 
-	vPos.push_back({{6,1.8,-1}, {0,1,0}, {0,0,1}});
-	//vPos.push_back(6); vPos.push_back(1.8); vPos.push_back(-1); //J'	//23
-	vPos.push_back({{-5.4,1.8,-1}, {0,1,0}, {0,0,1}});
-	//vPos.push_back(-5.4); vPos.push_back(1.8); vPos.push_back(-1); //R'
-	vPos.push_back({{-5.4,-0.2,-1}, {0,1,0}, {0,0,1}});
-	//vPos.push_back(-5.4); vPos.push_back(-0.2); vPos.push_back(-1); //Q' //AND G'
+	vPos.push_back({{6,1.8,0}, {0,1,0}, {0,0,1}});
+	//vPos.push_back(6); vPos.push_back(1.8); vPos.push_back(0); //J'	//23
+	vPos.push_back({{-5.4,1.8,0}, {0,1,0}, {0,0,1}});
+	//vPos.push_back(-5.4); vPos.push_back(1.8); vPos.push_back(0); //R'
+	vPos.push_back({{-5.4,-0.2,0}, {0,1,0}, {0,0,1}});
+	//vPos.push_back(-5.4); vPos.push_back(-0.2); vPos.push_back(0); //Q' //AND G'
 
-	vPos.push_back({{-5.4,-2.3,-1}, {0,1,0}, {0,0,1}});
-	//vPos.push_back(-5.4); vPos.push_back(-2.3); vPos.push_back(-1); //N'	//26
-	vPos.push_back({{-7.4,-2.3,-1}, {0,1,0}, {0,0,1}});
-	//vPos.push_back(-7.4); vPos.push_back(-2.3); vPos.push_back(-1); //K'
-	vPos.push_back({{-7.4,7.4,-1}, {0,1,0}, {0,0,1}});
-	//vPos.push_back(-7.4); vPos.push_back(7.4); vPos.push_back(-1); //O'
-	vPos.push_back({{-5.4,7.4,-1}, {0,1,0}, {0,0,1}});
-	//vPos.push_back(-5.4); vPos.push_back(7.4); vPos.push_back(-1); //P'	//29
+	vPos.push_back({{-5.4,-2.3,0}, {0,1,0}, {0,0,1}});
+	//vPos.push_back(-5.4); vPos.push_back(-2.3); vPos.push_back(0); //N'	//26
+	vPos.push_back({{-7.4,-2.3,0}, {0,1,0}, {0,0,1}});
+	//vPos.push_back(-7.4); vPos.push_back(-2.3); vPos.push_back(0); //K'
+	vPos.push_back({{-7.4,7.4,0}, {0,1,0}, {0,0,1}});
+	//vPos.push_back(-7.4); vPos.push_back(7.4); vPos.push_back(0); //O'
+	vPos.push_back({{-5.4,7.4,0}, {0,1,0}, {0,0,1}});
+	//vPos.push_back(-5.4); vPos.push_back(7.4); vPos.push_back(0); //P'	//29
 	
 	//ABCD 0 1 2 3
 	vIdx.push_back(0); vIdx.push_back(1); vIdx.push_back(2); //ABC
